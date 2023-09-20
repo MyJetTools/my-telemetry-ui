f5955494294b8ce6b6f7493433ac7fa52ba135e4
amigin <amigin@gmail.com>
2023-09-20 10:56:21 +0300

    Increased GRPC request timeout

--- src/grpc_client/telemetry_reader_grpc_client.rs
+++ src/grpc_client/telemetry_reader_grpc_client.rs
@@ -2,7 +2,7 @@
     proto_file: "./proto/TelemetryReader.proto",
     crate_ns: "crate::reader_grpc",
     retries: 3,
-    request_timeout_sec: 1,
+    request_timeout_sec: 20,
     ping_timeout_sec: 1,
     ping_interval_sec: 3,
 )]
