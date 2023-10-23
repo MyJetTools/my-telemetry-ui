d8499e2ec6d48509a39dc86ca8f64d7245276581
amigin <amigin@gmail.com>
2023-10-24 00:07:05 +0300

    Fixes

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,9 @@ lazy_static::lazy_static! {
 fn main() {
     let config = LaunchBuilder::<FullstackRouterConfig<AppRoute>>::router();
 
+    #[cfg(feature = "ssr")]
+    let config = config.addr(std::net::SocketAddr::from(([0, 0, 0, 0], 8080)));
+
     //let config = LaunchBuilder::new(app);
     /*
        #[cfg(feature = "ssr")]
